78fae0615f43a923ba69ae2e3ddf4315a5fddfc1
Neetu Seharawat <neetumorbca@gmail.com>
2023-01-31 22:55:16 +0530

    Validating the forms (UseEffect)- CollegeName added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-import AddUser from './components/Users/AddUser';
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(()=>{
+    const storedUserLoggedInInformation= localStorage.getItem('IsLoggedIn');
+     
+    if(storedUserLoggedInInformation ==='1'){
+      setIsLoggedIn(true);
+    }
+  },[]);
+  const loginHandler = (email, password, collegeName) => {
+        // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    localStorage.setItem('IsLoggedIn','1');
+    setIsLoggedIn(true);
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isloggedIn');
+    setIsLoggedIn(false);
+    };
+
   return (
-    <div>
-      <AddUser />
-    </div>
+    <React.Fragment>
+      <MainHeader isAuthenticated={isLoggedIn} onLogout={logoutHandler} />
+      <main>
+        {!isLoggedIn && <Login onLogin={loginHandler} />}
+        {isLoggedIn && <Home onLogout={logoutHandler} />}
+      </main>
+    </React.Fragment>
   );
 }
 
